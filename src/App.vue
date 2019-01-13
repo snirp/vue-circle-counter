e61152084b1b4cb4274333c7a6919f30fba8fd1e
Roy Prins <prinsroy@gmail.com>
2019-01-13 21:18:18 +0100

    Basic circle counter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <circle-counter width="20rem" height="20rem"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CircleCounter from '@/CircleCounter.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CircleCounter
   }
 }
 </script>
---
+++ src/CircleCounter.vue
@@ -0,0 +1,123 @@
+<template>
+  <svg :viewBox="`0 0 ${UNITS} ${UNITS}`" height="100%" width="100%">
+    <g :transform="`rotate(${rotate},${UNITS/2},${UNITS/2}) ${reverse ? 'scale(1,-1) translate(0, -' + UNITS +')' : ''}`">
+      <circle 
+        :cx="UNITS/2" 
+        :cy="UNITS/2" 
+        :r="getRadius()" 
+        :stroke="stroke" 
+        :stroke-width="getStrokeWidth(strokeWidth)" 
+        fill="none"
+        :stroke-dasharray="getLengths()"
+      />
+      <path
+        :d="describeArc(UNITS/2, UNITS/2, getRadius(), 0, activeEnd())" 
+        fill="none" 
+        :stroke="activeStroke" 
+        :stroke-width="getStrokeWidth(activeWidth)" 
+        :stroke-dasharray="getLengths()"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  beforeCreate() {
+    // Arbitrary dimensions of SVG to set up user-space units
+    this.UNITS = 32;
+  },
+  props: {
+    dashCount: {
+      type: Number,
+      default: 70
+    },
+    activeCount: {
+      type: Number,
+      default: 5
+    },
+    strokeWidth: {
+      type: Number, 
+      default: 10
+    },
+    activeWidth: {
+      type: Number, 
+      default: 10
+    },
+    stroke: {
+      type: String, 
+      default: 'lightgrey'
+    },
+    activeStroke: {
+      type: String,
+      default: 'dodgerblue'
+    },
+    dashSpacing: {
+      type: Number,
+      default: 0.5
+    },
+    rotate: {
+      type: Number,
+      default: -160
+    },
+    reverse: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    // Stroke is provided as a percentage of the radius, translate into user space units
+    getStrokeWidth(stroke){
+      return stroke*this.UNITS/200;
+    },
+
+    // Determine the 'end' angle of the path for the active dashes in degrees.
+    activeEnd(){
+      if (this.activeCount == 0){
+        return 0
+      }
+      return 360 * (this.activeCount*this.dashPerc() + (this.activeCount-1)*this.spacePerc())
+    },
+
+    // An array of the length of the dash & the length of the space between dashes
+    getLengths(){
+      return [2*Math.PI*this.getRadius()*this.dashPerc(), 2*Math.PI*this.getRadius()*this.spacePerc()]
+    },
+
+    // The space beween dashes as a percentage of the total length
+    spacePerc(){
+      return this.dashSpacing/this.dashCount;
+    },
+
+    // The length of a dash as a percentage of the total length
+    dashPerc(){
+      return (1-this.dashSpacing)/this.dashCount;
+    },
+
+    // Radius of the circle arc
+    getRadius(){
+      return this.UNITS/2 - Math.max(this.getStrokeWidth(this.strokeWidth), this.getStrokeWidth(this.activeWidth))
+    },
+
+    // SVG path definition requires points in cartesian space
+    polarToCartesian(cx, cy, radius, degrees) {
+      const radians = degrees * Math.PI / 180.0;
+      return {
+        x: cx + (radius * Math.cos(radians)),
+        y: cy + (radius * Math.sin(radians))
+      };
+    },
+
+    // Path definition for circular arc, clockwise when endDegrees
+    describeArc(cx, cy, radius, startDegrees, endDegrees){
+      const start = this.polarToCartesian(cx, cy, radius, startDegrees);
+      const end = this.polarToCartesian(cx, cy, radius, endDegrees);
+
+      let largeArc = (Math.abs(endDegrees-startDegrees) < 180) ? 0 : 1;
+      let sweep = (endDegrees < startDegrees) ? 0 : 1;
+
+      return `M${start.x} ${start.y} A${radius} ${radius} 0 ${largeArc} ${sweep} ${end.x} ${end.y}`
+    },
+  }
+}
+</script>
